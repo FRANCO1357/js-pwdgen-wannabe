9d6a82f560f7127e1f2a95767cef295579518eca
FRANCO1357 <fmelani13@gmail.com>
2022-06-20 16:14:21 +0200

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // 1 chiedere il nome
 // 2 chiedere il cognome
 // 3 chiedere il colore preferito
-// 4 sommare le risposte date 
-// 5 dare il risultato 
+// 4 sommare le risposte date ed aggiungere 21
+// 5 dare il risultato nel file html
+
+const FirstName = prompt('Quale è il tuo nome?');
+
+const LastName = prompt('Quale è il tuo cognome?');
+
+const FavouriteColor = prompt('Quale è il tuo colore preferito?');
+
+const password = FirstName + LastName + FavouriteColor + 21
+
+const paragraph = document.getElementById('password-paragraph');
+
+paragraph.innerText = 'Ciao ' + FirstName + ', la tua password è: ' + password;
